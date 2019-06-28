98d0909e90411ddf9b33ccf58ba02a82c26cf22c
dshufeng <839117348@qq.com>
2019-06-28 16:22:51 +0800

    config assets

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 module.exports = {
-    publicPath: './', // 默认'/'，部署应用包时的基本 URL
+    publicPath: '/', // 默认'/'，部署应用包时的基本 URL
     outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
-    assetsDir: './',  // 相对于outputDir的静态资源(js、css、img、fonts)目录
+    assetsDir: 'assets',  // 相对于outputDir的静态资源(js、css、img、fonts)目录
     lintOnSave: false,
     runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
     productionSourceMap: true,  // 生产环境的 source map
